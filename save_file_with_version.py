8ba0532e1a604e042b27881e107cf33cd06a3952
HoscarConHache <80824491+HoscarCT@users.noreply.github.com>
2023-01-11 14:57:20 +0100

    Saving script added, loading script pending

---
+++ save_file_with_version.py
@@ -0,0 +1,137 @@
+from PySide2 import QtWidgets, QtCore
+import sys, os, glob
+import hou
+
+class SaveFileApp(QtWidgets.QWidget):
+
+    def __init__(self):
+        super(SaveFileApp, self).__init__()
+        self.fileCollection = []
+        self.version = 0
+        self.initUI()
+        self.checkDir()
+
+    def initUI(self):
+        #======================FILEPATH======================
+        self.filePathLabel = QtWidgets.QLabel("File Path:   ")
+
+        self.filePathTextBox = QtWidgets.QLineEdit(hou.homeHoudiniDirectory() if hou.hipFile.isNewFile() else os.path.dirname(hou.hipFile.path()))
+        self.filePathTextBox.textChanged.connect(self.checkDir)
+
+        self.buttonBrowse = QtWidgets.QPushButton("Browse")
+        self.buttonBrowse.clicked.connect(self.browsePath)
+        
+        self.filePathHLayout = QtWidgets.QHBoxLayout()
+        self.filePathHLayout.addWidget(self.filePathLabel)
+        self.filePathHLayout.addWidget(self.filePathTextBox)
+        self.filePathHLayout.addWidget(self.buttonBrowse)
+
+        #======================FILENAME======================
+        self.fileNameLabel = QtWidgets.QLabel("File Name: ")
+
+        self.fileNameTextBox = QtWidgets.QLineEdit(os.path.splitext(hou.hipFile.basename())[0].rsplit('_v')[0])
+        self.fileNameTextBox.textChanged.connect(self.checkFile)
+        
+        self.extensionLabel = QtWidgets.QLabel(".hip")
+        self.extensionLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.extensionLabel.setStyleSheet("margin-left: 0px; margin-right: 5px")
+        
+        self.versionLabel = QtWidgets.QLabel("Version:    ")
+        self.versionLabel.setAlignment(QtCore.Qt.AlignCenter)
+        self.versionLabel.setStyleSheet("color: #cccccc; background-color: #4b4b4b; border-radius: 5px; padding-left: 5px; padding-right: 5px")
+
+        self.fileNameHLayout = QtWidgets.QHBoxLayout()
+        self.fileNameHLayout.addWidget(self.fileNameLabel)
+        self.fileNameHLayout.addWidget(self.fileNameTextBox)
+        self.fileNameHLayout.addWidget(self.extensionLabel)
+        self.fileNameHLayout.addWidget(self.versionLabel)
+
+        #======================COMMENTS======================
+        self.commentsLabel = QtWidgets.QLabel("Comments:")
+        self.commentsLabel.setAlignment(QtCore.Qt.AlignTop)
+
+        self.commentsTextBox = QtWidgets.QTextEdit()
+        
+        self.commentsHLayout = QtWidgets.QHBoxLayout()
+        self.commentsHLayout.addWidget(self.commentsLabel)
+        self.commentsHLayout.addWidget(self.commentsTextBox)
+
+        #======================SAVE======================
+        self.buttonSave = QtWidgets.QPushButton("Save")
+        self.buttonSave.clicked.connect(self.saveFile)
+
+        self.mainVLayout = QtWidgets.QVBoxLayout()
+        self.mainVLayout.addLayout(self.filePathHLayout)
+        self.mainVLayout.addLayout(self.fileNameHLayout)
+        self.mainVLayout.addLayout(self.commentsHLayout)
+        self.mainVLayout.addWidget(self.buttonSave)
+
+        self.setLayout(self.mainVLayout)
+        self.setMinimumSize(500, 250)
+        self.setWindowTitle("Save File")
+
+    def saveFile(self):
+        #Check fields are not empty
+        if(not self.filePathTextBox.text() or not self.fileNameTextBox.text()):
+            QtWidgets.QMessageBox.warning(self, 
+                'Empty Fields',
+                "Please, fill all fields",
+                 QtWidgets.QMessageBox.Ok)
+            return
+
+        #File path exists
+        if(not os.path.exists(self.filePathTextBox.text())):
+            os.mkdir(self.filePathTextBox.text())
+
+        #Check invalid chars
+        invalid_chars = ['\\', '/', ':', '*', '?', '<', '>', '|']
+        if(any(char in self.fileNameTextBox.text() for char in invalid_chars)):
+            QtWidgets.QMessageBox.warning(self, 
+                'Invalid File Name',
+                "File name cannot contain any of the following charcaters:\n"
+                + "    \\ / : * ? < > |    ",
+                 QtWidgets.QMessageBox.Ok,
+            )
+            return
+
+        textFilePath = os.path.join(
+            self.filePathTextBox.text(),
+            self.fileNameTextBox.text() +
+            '_v' + format(self.version, '03d') +
+            '.hip'
+        )
+
+        hou.hipFile.setName(textFilePath)
+        hou.hipFile.save(textFilePath)
+
+        QtWidgets.QMessageBox.information(self, 
+                'File saved',
+                "File name: {0}\n".format(self.fileNameTextBox.text()) +
+                "Version: {0}".format(self.version),
+                 QtWidgets.QMessageBox.Ok)
+
+        self.checkDir()
+    
+    def browsePath(self):
+        folder = str(QtWidgets.QFileDialog.getExistingDirectory(self, "Select Directory"))
+        self.filePathTextBox.setText(folder)
+
+    def checkDir(self):
+        if(os.path.exists(self.filePathTextBox.text())):
+            self.fileCollection = glob.glob(os.path.join(self.filePathTextBox.text(), "*.hip"))
+        self.checkFile()
+
+    def checkFile(self):
+        if(not self.fileNameTextBox.text()):
+            return
+        self.version = 1
+        versionfiles = [f for f in self.fileCollection if (self.fileNameTextBox.text() + '_v' in f and os.path.basename(f).startswith(self.fileNameTextBox.text() + '_v'))]
+        if(versionfiles):
+            versions = [int(os.path.splitext(os.path.basename(v))[0].split('_v')[-1]) for v in versionfiles]
+            self.version = max(versions) + 1
+        
+        self.versionLabel.setText("Version: " + format(self.version, '03d'))
+
+#Run app
+app = SaveFileApp()
+app.show()
