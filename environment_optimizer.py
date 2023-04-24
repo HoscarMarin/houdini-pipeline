20b7201c9c4c47f0b12ae08a51d9533df2c9400b
HoscarCT <oscartalbuma@gmail.com>
2023-04-24 17:37:09 +0200

    environment_optimyzer v0.2

--- environment_optimizer.py
+++ environment_optimizer.py
@@ -1,48 +1,51 @@
 import hou
 import sys
 
-if __name__ == "__main__":
-
-    #Args handling
-    file_path = sys.argv[1]
+def readParams():
     if(len(sys.argv) > 1):
         file_path = sys.argv[1]
         if(len(sys.argv) > 2):
             percentage = float(sys.argv[2])
         else:
             percentage = 20.0
+        return file_path, percentage
     else:
         print("File Path is needed as the first argument.\nExample execution:\n\tpython environment_optimizer.py C:/path/file.hip (20)")
+        return "", -1
 
-    #File handling
-    try:
-        hou.hipFile.load(file_path)
-        scene = hou.node("/obj")
+if __name__ == "__main__":
+    
+    file_path, percentage = readParams()
 
-        #Take the all GEO Nodes in the scene
-        geo_nodes = list(filter(
-                        lambda node: node.type().name()== 'geo',
-                        scene.children()
-                    ))
-        
-        for geo_node in geo_nodes:
-        #Get which one is being displayed
-            displayNode = list(filter(
-                            lambda node: node.isGenericFlagSet(hou.nodeFlag.Display),
-                            geo_node.children()
-                        ))[0]
+    if(file_path != ""):
+        try:
+            hou.hipFile.load(file_path)
+            scene = hou.node("/obj")
+
+            #Take the all GEO Nodes in the scene
+            geo_nodes = list(filter(
+                            lambda node: node.type().name()== 'geo',
+                            scene.children()
+                        ))
             
-            prNode = geo_node.createNode("polyreduce::2.0")
-            prNode.parm("percentage").set(percentage)
+            for geo_node in geo_nodes:
+            #Get which one is being displayed
+                displayNode = list(filter(
+                                lambda node: node.isGenericFlagSet(hou.nodeFlag.Display),
+                                geo_node.children()
+                            ))[0]
+                
+                prNode = geo_node.createNode("polyreduce::2.0")
+                prNode.parm("percentage").set(percentage)
 
-            prNode.setFirstInput(displayNode)
-            prNode.setGenericFlag(hou.nodeFlag.Display, True)
-            #prNode.setGenericFlag(hou.nodeFlag.Render, True)
+                prNode.setFirstInput(displayNode)
+                prNode.setGenericFlag(hou.nodeFlag.Display, True)
+                #prNode.setGenericFlag(hou.nodeFlag.Render, True)
 
-        hou.hipFile.save()
+            hou.hipFile.save()
 
-    except hou.OperationFailed:
-        print(f"Failed to load file: {file_path}")
-        
-    except Exception as e:
-        print(f"Error: {e}")
+        except hou.OperationFailed:
+            print(f"Failed to load file: {file_path}")
+            
+        except Exception as e:
+            print(f"Error: {e}")
